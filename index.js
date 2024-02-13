3645d9a7193b509e4546131879a5302546e2fa79
NadyaEvdokimova <146958813+NadyaEvdokimova@users.noreply.github.com>
2024-02-13 15:58:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const password = process.env.password;
 
 const db = new pg.Client({
   user: "postgres",
-  host: "postgres://world_icib_user:WtvBLYiZJ459KeHCMOUeNwhyJNQ6sJxp@dpg-cn5o4bect0pc738hdm3g-a/world_icib",
+  host: "localhost",
   database: "world",
   password: password,
   port: 5432,
